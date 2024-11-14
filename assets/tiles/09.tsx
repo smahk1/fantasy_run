85cfed595d6992dc4436c851413ee910874dfc0b
ali-kyun <smahk1122@gmail.com>
2024-11-14 19:01:29 +0500

    Game window created
    Added the following:
    -Flame dependencies in PUBSPEC
    -Made the game window (no functionality)
    -Created games test map (yet to add)

---
+++ assets/tiles/09.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="09" tilewidth="16" tileheight="16" tilecount="380" columns="19">
+ <image source="../images/Presets/09.png" width="304" height="330"/>
+</tileset>
