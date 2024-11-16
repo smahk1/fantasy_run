0b527e8558238ea05863b46b633fb281dbba1af7
AliHassan <232406@students.au.edu.pk>
2024-11-16 21:19:08 +0500

    assets
    updated assets and added relevant running character with rainforest layers background

--- assets/tiles/08.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="08" tilewidth="16" tileheight="16" tilecount="380" columns="19">
- <image source="../images/Presets/08.png" width="304" height="330"/>
-</tileset>
