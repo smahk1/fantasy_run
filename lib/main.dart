99db3ed027d83cef68e3269e8f8bcb8578307fe9
ali-kyun <smahk1122@gmail.com>
2024-11-26 19:06:23 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:fantasy_run/fantasy_run.dart';
 import 'package:flame/game.dart';
-import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
-  Flame.device.setLandscape();
+  FantasyRun fantasyRun = FantasyRun();
 
-  // using FantasyRun to make "game" object as a game window
-  FantasyRun game = FantasyRun();
-  // recreate game every time a change is made instead of hot restarting[for testing] -- disbale when not in testing
-  runApp(GameWidget(game: kDebugMode ? FantasyRun() : game));
+  runApp(GameWidget(game: fantasyRun));
 }
